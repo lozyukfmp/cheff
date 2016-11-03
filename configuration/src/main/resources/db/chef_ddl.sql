7b047af2ab3c82e370cd66626d3ec261982e0232
lozyukfmp <lozyukfmp@users.noreply.github.com>
2016-11-03 14:01:25 +0300

    Update chef_ddl.sql

--- configuration/src/main/resources/db/chef_ddl.sql
+++ configuration/src/main/resources/db/chef_ddl.sql
@@ -1,43 +1,48 @@
-INSERT INTO `chef`.`menu` (`menu_id`, `menu_name`) VALUES (NULL, 'Путешествие во времени');
-INSERT INTO `chef`.`menu` (`menu_id`, `menu_name`) VALUES (NULL, 'Заморское');
-INSERT INTO `chef`.`menu` (`menu_id`, `menu_name`) VALUES (NULL, 'Бизнес');
-INSERT INTO `chef`.`menu` (`menu_id`, `menu_name`) VALUES (NULL, 'Восточное');
-INSERT INTO `chef`.`menu` (`menu_id`, `menu_name`) VALUES (NULL, 'Традиционное');
-INSERT INTO `chef`.`menu` (`menu_id`, `menu_name`) VALUES (NULL, 'Китайское');
-INSERT INTO `chef`.`menu` (`menu_id`, `menu_name`) VALUES (NULL, 'Домашнее');
+CREATE DATABASE `chef` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Оливье');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Осенний');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Вьюга');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Любимый');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Агата');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Греческий');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Свекольный');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Лаура');
-INSERT INTO `chef`.`salad` (`salad_id`, `salad_name`) VALUES(NULL, 'Арфа');
+CREATE TABLE `chef`.`menu` (
+  `menu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`menu_id`),
+  UNIQUE KEY `menu_name_UNIQUE` (`menu_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Картофель', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Баклажан', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Редис', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Чеснок', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Капуста', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Морковь', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Помидор', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Огурец', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Лук', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Свекла', 23.2, 5.2, 1.2, 0.8);
-INSERT INTO `chef`.`vegetable` (`vegetable_id`,`vegetable_name`,`calories`,`fats`,`proteins`,`carbohydrates`) 
-VALUES (NULL, 'Кабачок', 23.2, 5.2, 1.2, 0.8);
+CREATE TABLE `chef`.`salad` (
+  `salad_id` int(11) NOT NULL AUTO_INCREMENT,
+  `salad_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`salad_id`),
+  UNIQUE KEY `salad_name_UNIQUE` (`salad_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `chef`.`vegetable` (
+  `vegetable_id` int(11) NOT NULL AUTO_INCREMENT,
+  `vegetable_name` varchar(50) NOT NULL,
+  `calories` double NOT NULL,
+  `fats` double NOT NULL,
+  `proteins` double NOT NULL,
+  `carbohydrates` double NOT NULL,
+  PRIMARY KEY (`vegetable_id`),
+  UNIQUE KEY `vegetable_name_UNIQUE` (`vegetable_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `chef`.`menu_m2m_salad` (
+  `menu_id` int(11) NOT NULL,
+  `salad_id` int(11) NOT NULL,
+  KEY `fk_menu_id` (`menu_id`),
+  KEY `fk_salad_id` (`salad_id`),
+  CONSTRAINT `fk_menu_id` FOREIGN KEY (`menu_id`) REFERENCES `chef`.`menu` (`menu_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_salad_id` FOREIGN KEY (`salad_id`) REFERENCES `chef`.`salad` (`salad_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `chef`.`salad_m2m_vegetable` (
+  `salad_id` int(11) NOT NULL,
+  `vegetable_id` int(11) NOT NULL,
+  PRIMARY KEY (`salad_id`,`vegetable_id`),
+  KEY `fk_vegetable_id_idx` (`vegetable_id`),
+  CONSTRAINT `fk_salat_id` FOREIGN KEY (`salad_id`) REFERENCES `chef`.`salad` (`salad_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_vegetable_id` FOREIGN KEY (`vegetable_id`) REFERENCES `chef`.`vegetable` (`vegetable_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
